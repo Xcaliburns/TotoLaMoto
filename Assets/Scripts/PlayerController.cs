d3fad0683a18f442a0aede9ef33b6ec0b9120186
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-12-26 18:09:31 +0100

    jitter resolved

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,31 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
     public float speed = 10f;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * speed *Time.deltaTime;
-        transform.position = position;
+      
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
 
