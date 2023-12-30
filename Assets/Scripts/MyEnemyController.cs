462012ad61f0c43633fe65832c2df0b34b799bab
David Abruzzo <64249039+Xcaliburns@users.noreply.github.com>
2023-12-30 12:04:51 +0100

    added simple enemy animations

--- Assets/Scripts/MyEnemyController.cs
+++ Assets/Scripts/MyEnemyController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MyEnemyController : MonoBehaviour
@@ -16,40 +14,43 @@ public class MyEnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         timer = changeTime;
     }
 
-   
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         timer -= Time.deltaTime;
+
 
         if (timer < 0)
         {
             direction = -direction;
             timer = changeTime;
         }
-    }
 
-    private void FixedUpdate()
-    {
         Vector2 position = rigidbody2d.position;
 
         if (vertical)
         {
             position.y += speed * Time.deltaTime * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
 
         }
         else
         {
 
             position.x += speed * Time.deltaTime * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2d.MovePosition(position);
@@ -57,10 +58,10 @@ public class MyEnemyController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-       PlayerController player = other.gameObject.GetComponent<PlayerController>();
-        if (player != null) 
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if (player != null)
         {
-            player.ChangeHealth(-1);        
+            player.ChangeHealth(-1);
         }
     }
 }
